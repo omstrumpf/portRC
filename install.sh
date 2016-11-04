71d35d0c7c4cbe5ccde2a2f4ea77b0433acf9162
Omer Strumpf <omer@Omers-MacBook-Pro.local>
2016-11-04 01:06:43 -0400

    added tmux pluggins

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-rm -f ~/.vimrc
-rm -f ~/.bashrc
 rm -rf ~/.vim
+rm -rf ~/.tmux
 cp vimrc ~/.vimrc
 cp bashrc ~/.bashrc
 cp -r vim ~/.vim
 cp profile ~/.profile
+cp -r tmux ~/.tmux
 cp tmux.conf ~/.tmux.conf
 if [ ! -f ~/.bashprompttag ]; then
   cp prompttag ~/.bashprompttag
